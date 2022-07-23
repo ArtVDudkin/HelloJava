f9e876824b4bc1a0ef0c1fdb8f60007cf6337d5e
Artem <kardan97@yandex.ru>
2022-07-23 17:43:34 +0700

    OOP Messager. Abstract MessageModel added

--- OOP_DZ_5/Messager/Server/AudioMessage.java
+++ OOP_DZ_5/Messager/Server/AudioMessage.java
@@ -2,4 +2,12 @@ package Messager.Server;
 
 public class AudioMessage extends MessageModel{
     
+    private AudioMessage voice;
+    private String title;
+
+    public AudioMessage(AudioMessage voice) {
+        super.text = title;
+        super.id++;
+    }
+
 }
