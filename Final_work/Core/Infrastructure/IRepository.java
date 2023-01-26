95c90fbf0134114f2818de4dadedc525684d38fc
Artem <kardan97@yandex.ru>
2023-01-26 20:47:29 +0700

    Repository interface and class added

--- Final_work/Core/Infrastructure/IRepository.java
+++ Final_work/Core/Infrastructure/IRepository.java
@@ -6,6 +6,20 @@ import Final_work.Core.Models.Animal;
 
 public interface IRepository {
     
-   
+    void add(Animal animal);
+
+    void remove(Animal animal);
+
+    Animal getById(int id);
+
+    Animal getByName(String name);
+
+    int getCount();
+
+    List<Animal> getAnimalList();
+    
+    void save();
+
+    void load();
 
 }
