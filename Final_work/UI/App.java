8d0c8d1f8bd40a7da9e9fcd781d73db727b4e97c
Artem <kardan97@yandex.ru>
2023-01-26 20:52:38 +0700

    ConsoleView and menu navigation added

--- Final_work/UI/App.java
+++ Final_work/UI/App.java
@@ -1,9 +1,11 @@
 package Final_work.UI;
 
+import java.time.LocalDate;
+import java.util.Scanner;
 
 import Final_work.Core.MVC.Controller.Controller;
 import Final_work.Core.MVC.Model.Model;
-
+import Final_work.Core.Models.Types.Dog;
 
 public class App {
     
@@ -11,7 +13,44 @@ public class App {
        
     ConsoleView view = new ConsoleView();
     Controller controller = new Controller(new Model(), view);
-    
+    //controller.loadFromFile();
+    controller.addAnimal(new Dog("Grey", LocalDate.now()));
+
+        try (Scanner in = new Scanner(System.in)) {
+            while (true) {
+                view.showMenu(0);
+                String key = in.next();
+                view.refresh();
+                switch (key) {
+                    case "1":
+                        controller.prev();
+                        break;
+                    case "2":
+                        controller.next();
+                        break;
+                    case "3":
+                        controller.add();
+                        break;
+                    case "4":
+                        controller.remove();
+                        break;
+                    case "5":
+                        controller.showCommands();
+                        break;
+                    case "6":
+                        controller.addCommand();
+                        break;
+                    case "7":
+                        controller.save();
+                        break;
+                    case "8":
+                        System.exit(0);
+                    default:
+                        System.out.println("Такой команды нет");
+                        break;
+                }
+            }
+        }
 
     }
 }
--- Final_work/UI/ConsoleView.java
+++ Final_work/UI/ConsoleView.java
@@ -15,10 +15,45 @@ public class ConsoleView implements IView {
     }
 
     @Override
+    public void refresh() {
+        System.out.print("\033[H\033[J");
+    }
+
+    @Override
     public void showMenu(int menu) {
-        // TODO Auto-generated method stub
+        System.out.println(" 1 - prev  2 - next  3 - add new animal  4 - remove animal  5 - show commands  6 - add new command  7 - save  8 - exit");
+            
+    }
+
+    @Override
+    public void setCurrIndex(int value) {
+        System.out.printf("Current note: %d\n", value + 1);
+    }
+
+    private Scanner in = new Scanner(System.in);
+
+    @Override
+    public String inputText(String text) {
+        System.out.printf("%s", text);
+        return in.nextLine();
+    }
+
+    @Override
+    public void printAnimals(Repository animals) {
+        System.out.println("Список животных в приюте на " + LocalDate.now());
+        System.out.println("id   Type\t Class\t Name\t Birthday");  
+        for (Animal animal : animals.getAnimalList()) {
+            System.out.printf("%d    %s\t %s\t%s\t%s\n", 
+                1, "cat", animal.getClass(),  animal.getBirthday());       
+        };
         
     }
 
+
+    @Override
+    public void print(String value) {
+        System.out.println(value);
+        
+    }
     
 }
