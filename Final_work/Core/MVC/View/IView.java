67ea732f4aacd6c03509ff24b008bfa23b94b2d2
Artem <kardan97@yandex.ru>
2023-01-26 20:44:50 +0700

    Interface IView added

--- Final_work/Core/MVC/View/IView.java
+++ Final_work/Core/MVC/View/IView.java
@@ -1,6 +1,14 @@
 package Final_work.Core.MVC.View;
 
+import Final_work.Core.Infrastructure.Repository;
+
 public interface IView {
+    
     void showMenu(int menu);
+    void refresh();
+    void setCurrIndex(int value);
+    void printAnimals(Repository animals);
+    void print(String value);
+    String inputText(String text);
 
 }
