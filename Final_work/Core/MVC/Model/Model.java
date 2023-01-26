85e2ca5e2bfcacb59881b4becaccbb0f0cf7e1c2
Artem <kardan97@yandex.ru>
2023-01-26 20:48:58 +0700

    Class Model added

--- Final_work/Core/MVC/Model/Model.java
+++ Final_work/Core/MVC/Model/Model.java
@@ -1,7 +1,9 @@
 package Final_work.Core.MVC.Model;
 
+import java.util.List;
 
 import Final_work.Core.Infrastructure.Repository;
+import Final_work.Core.Models.Animal;
 
 public class Model {
 
@@ -9,6 +11,50 @@ public class Model {
 
     public Model() {
         repo = new Repository();
+    }
+
+    public void add(Animal animal) {
+        repo.add(animal);
+        System.out.println(animal.getCommands());
+    }
+
+    public void remove(Animal animal) {
+        repo.remove(animal);
+    }
+
+    public Animal getByName(String name) {
+        if(repo != null) {
+            return repo.getByName(name);
+        }
+        return null;
+    }
+
+    public Animal getById(int id) {
+        if(id >= 0 && id < repo.getCount()) {
+            return repo.getById(id);
+        }
+        return null;
+    }
+
+    public Repository getRepo() {
+        if(repo != null) {
+            return repo;
+        }
+        return null;
+    }
+
+    public List<Animal> getAnimalList() {
+        if(repo != null) {
+            return repo.getAnimalList();
+        }
+        return null;
+    }
+
+    public int getCount() {
+        if(repo != null) {
+            return repo.getCount();
+        }
+        return 0; 
     }
     
 }
